62e176d0174313c3694e4a67f48020c934bae6cc
ekrem-kocak <ekrem-kocak@outlook.com>
2026-02-05 23:28:25 +0300

    feat(prisma): add seeding functionality and update configuration

--- libs/api/data-access-db/prisma.config.ts
+++ libs/api/data-access-db/prisma.config.ts
@@ -1,15 +1,17 @@
 import { config } from 'dotenv';
 import { resolve } from 'path';
-import { defineConfig } from 'prisma/config';
+import { defineConfig, env } from 'prisma/config';
 
+// Environment variables'ı yükle
 config({ path: resolve(__dirname, '../../../.env') });
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
   },
   datasource: {
-    url: process.env['DIRECT_URL'],
+    url: env('DIRECT_URL'),
   },
 });
